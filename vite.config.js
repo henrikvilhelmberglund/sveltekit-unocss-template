d96ac434f0d729c37aacdfaf8fe584dc607304cd
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-02-09 13:58:36 +0100

    feat: :sparkles: make it more obvious that preset-uno is used by default

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,14 @@
 import { sveltekit } from "@sveltejs/kit/vite";
 import UnoCSS from "unocss/vite";
+import presetUno from "@unocss/preset-uno";
 import { defineConfig } from "vite";
 
 export default defineConfig({
 	plugins: [
 		UnoCSS({
-			mode: "svelte-scoped"
+			presets: [presetUno()],
+			mode: "svelte-scoped",
 		}),
-		sveltekit()
-	]
+		sveltekit(),
+	],
 });
