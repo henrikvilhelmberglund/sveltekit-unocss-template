812c9a8d9d50c014ae7c090956745a39943f1224
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-02-09 14:11:44 +0100

    feat: :sparkles: update index route

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,46 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	const contents = [
+		{
+			name: "SvelteKit",
+			link: "https://kit.svelte.dev",
+			desc: "with adapter-static for easier Github Pages deployment",
+		},
+		{
+			name: "UnoCSS",
+			link: "https://github.com/unocss/unocss",
+			desc: "using Svelte scoped mode",
+		},
+		{
+			name: "Prettier Plugin TailwindCSS",
+			link: "https://github.com/tailwindlabs/prettier-plugin-tailwindcss",
+			desc: "for auto sorting of classes",
+		},
+		{
+			name: "Commit-and-Tag-Version",
+			link: "https://github.com/absolute-version/commit-and-tag-version",
+			desc: "for CHANGELOG generation and Github tagging of versions",
+		},
+	];
+</script>
+
+<main class="[&>*]:m-4">
+	<h1 class="text-4xl">Welcome to SvelteKit - UnoCSS template!</h1>
+	<p>
+		Visit <a
+			class="underline-blue-600 text-2xl underline hover:text-blue-600"
+			href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation for SvelteKit
+	</p>
+	<p>This template includes:</p>
+	<ul>
+		{#each contents as { name, link, desc }}
+			<li class="list-none">
+				<div class="flex flex-col items-start pb-4 font-serif">
+					<a class="underline-blue-600 text-2xl underline hover:text-blue-600" href={link}>
+						{name}
+					</a>
+					<span>{desc}</span>
+				</div>
+			</li>
+		{/each}
+	</ul>
+</main>
