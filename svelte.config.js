9e7d48e6a1458d153e5bed6d8a2839e303f2eb60
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-02-09 13:40:12 +0100

    feat: :sparkles: use adapter-static by default

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,18 @@
-import adapter from '@sveltejs/adapter-auto';
+// use adapter-auto for zero config deployment to Vercel, adapter-static for Github Pages
+// import adapter from '@sveltejs/adapter-auto';
+import adapter from "@sveltejs/adapter-static";
+
+const dev = process.argv.includes("dev");
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter()
-	}
+		adapter: adapter(),
+		paths: {
+			// change path below to your github repo if using Github Pages
+			base: dev ? "" : "/sveltekit-unocss-template",
+		},
+	},
 };
 
 export default config;
