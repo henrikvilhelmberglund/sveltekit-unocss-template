d8f6c659d626a9b223a6cc64e07e08e639331a47
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-03-22 08:02:22 +0100

    feat: :sparkles: split into components

---
+++ src/lib/Footer.svelte
@@ -0,0 +1,13 @@
+<script>
+	import { base } from "$app/paths";
+</script>
+
+<footer class="m-4 h-64">
+	<p>
+		by
+		<a
+			class="underline-blue-600 underline hover:text-blue-600"
+			href="https://github.com/henrikvilhelmberglund">
+			<img class="inline w-6" src="{base}/Henrik.png" alt="avatar" />henrikvilhelmberglund</a>
+	</p>
+</footer>
---
+++ src/lib/Welcome.svelte
@@ -0,0 +1,53 @@
+<script>
+	import { base } from "$app/paths";
+	const contents = [
+		{
+			name: "SvelteKit",
+			link: "https://kit.svelte.dev",
+			desc: "with adapter-static for easier Github Pages deployment",
+		},
+		{
+			name: "UnoCSS",
+			link: "https://github.com/unocss/unocss",
+			desc: "using Svelte scoped mode, includes various plugins (see vite.config.js)",
+		},
+		{
+			name: "Prettier Plugin TailwindCSS",
+			link: "https://github.com/tailwindlabs/prettier-plugin-tailwindcss",
+			desc: "for auto sorting of classes",
+		},
+		{
+			name: "Commit-and-Tag-Version",
+			link: "https://github.com/absolute-version/commit-and-tag-version",
+			desc: "for CHANGELOG generation and Github tagging of versions",
+		},
+		{
+			name: "PrismJS",
+			link: "https://prismjs.com",
+			desc: "with Prism-Svelte for syntax highlighting of code blocks",
+		},
+	];
+</script>
+
+<h1 class="text-4xl">Welcome to SvelteKit - UnoCSS template!</h1>
+<p>
+	Visit <a class="underline-blue-600 underline hover:text-blue-600" href="https://kit.svelte.dev"
+		>kit.svelte.dev</a> to read the documentation for SvelteKit
+</p>
+<div class="i-noto-cat-face text-6xl" />
+<p>This template includes:</p>
+<ul>
+	{#each contents as { name, link, desc }}
+		<li class="list-none">
+			<div class="flex flex-col items-start pb-4 font-serif">
+				<a class="underline-blue-600 text-2xl underline hover:text-blue-600" href={link}>
+					{name}
+				</a>
+				<span>{desc}</span>
+			</div>
+		</li>
+	{/each}
+</ul>
+
+<style>
+</style>
