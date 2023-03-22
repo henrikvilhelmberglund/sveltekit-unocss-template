522a09f9fd8d97a642caa9382a30b9f9d89aa571
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-03-22 07:23:52 +0100

    feat: :package: add @unocss/preset-web-fonts

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,36 @@
 import { sveltekit } from "@sveltejs/kit/vite";
 import UnoCSS from "unocss/vite";
 import presetUno from "@unocss/preset-uno";
+import presetWebFonts from "@unocss/preset-web-fonts";
 import { defineConfig } from "vite";
 
 export default defineConfig({
 	plugins: [
 		UnoCSS({
-			presets: [presetUno()],
+			presets: [
+				presetUno(),
+				presetWebFonts({
+					provider: "google", // default provider
+					fonts: {
+						// these will extend the default theme
+						sans: "Roboto",
+						mono: ["Fira Code", "Fira Mono:400,700"],
+						// custom ones
+						lobster: "Lobster",
+						lato: [
+							{
+								name: "Lato",
+								weights: ["400", "700"],
+								italic: true,
+							},
+							{
+								name: "sans-serif",
+								provider: "none",
+							},
+						],
+					},
+				}),
+			],
 			mode: "svelte-scoped",
 		}),
 		sveltekit(),
