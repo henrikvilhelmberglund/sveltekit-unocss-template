54dc04ad1d40be7ba33bd7b2af2c2906da5abf43
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-04-21 00:53:12 +0200

    fix: :fire: remove component import

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,10 @@
 <script>
 	import { base } from "$app/paths";
 	import Footer from "$lib/Footer.svelte";
-	import Welcome from "$lib/Welcome.svelte";
 </script>
 
 <main class="[&>*]:m-4">
-
+Hello
 </main>
 
 <Footer />
