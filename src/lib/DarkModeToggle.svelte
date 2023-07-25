c7ce301bdd1b652eda836e4bb38c38fc3173d7cb
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-07-25 13:05:55 +0200

    feat: :sparkles: add DarkModeToggle component

---
+++ src/lib/DarkModeToggle.svelte
@@ -0,0 +1,35 @@
+<script>
+	import { onMount } from "svelte";
+	let darkMode;
+	onMount(() => {
+		darkMode = localStorage.theme === "dark" ? true : false;
+	});
+
+	function toggle() {
+		darkMode = !darkMode;
+		// Tailwind dark mode
+		// On page load or when changing themes, best to add inline in `head` to avoid FOUC
+		if (!darkMode) {
+			// Whenever the user explicitly chooses light mode
+			localStorage.theme = "light";
+		}
+		if (darkMode) {
+			// Whenever the user explicitly chooses dark mode
+			localStorage.theme = "dark";
+		}
+
+		if (localStorage.theme === "dark" || !("theme" in localStorage)) {
+			document.documentElement.classList.add("dark");
+		} else {
+			document.documentElement.classList.remove("dark");
+		}
+
+		// Whenever the user explicitly chooses to respect the OS preference
+		// localStorage.removeItem("theme");
+	}
+</script>
+
+<button on:click={() => toggle()} class="i-carbon-sun dark:i-carbon-moon" />
+
+<style>
+</style>
