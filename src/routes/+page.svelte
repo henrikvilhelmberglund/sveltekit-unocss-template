89697769c53e7b337a62df6c35d181ac604a6054
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-07-25 13:06:47 +0200

    feat: :sparkles: add theme switching (should make this a component)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,33 @@
 <script>
-	import { afterUpdate } from "svelte";
+	import { onMount } from "svelte";
 	import { base } from "$app/paths";
 	import Footer from "$lib/Footer.svelte";
+	import DarkModeToggle from "$lib/DarkModeToggle.svelte";
+	import { browser } from "$app/environment";
+
+	let selectedTheme;
+
+	$: {
+		if (browser) {
+			if (selectedTheme) {
+				themes.forEach((theme) => {
+					document.documentElement.classList.remove(theme);
+				});
+				localStorage.color = selectedTheme;
+				document.documentElement.classList.add(selectedTheme);
+			}
+		}
+	}
+
+	let themes = ["blue", "yellow"];
 </script>
 
-<main class="[&>*]:m-4">
-	<p class="p-48 text-9xl">Hello</p>
+<main class="bg-primary-100 dark:bg-primary-900 [&>*]:m-4">
+	{#each themes as theme}
+		<button on:click={() => (selectedTheme = theme)} class="btn-primary">{theme}</button>
+	{/each}
+	<DarkModeToggle />
+	<h1 class="text-primary-900 dark:text-primary-100 text-9xl md:p-48">Hello!</h1>
 </main>
 
 <Footer />
