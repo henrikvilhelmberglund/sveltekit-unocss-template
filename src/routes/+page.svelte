799d44388e4704a16e88d39fc65f00c95fa3557c
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-07-25 16:19:17 +0200

    fix: :lipstick: fix styling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,12 +4,19 @@
 	import Footer from "$lib/Footer.svelte";
 	import DarkModeToggle from "$lib/theme/DarkModeToggle.svelte";
 	import ThemeSwitcher from "$lib/theme/ThemeSwitcher.svelte";
+	import { color } from "$lib/stores";
 </script>
 
-<main class="bg-primary-100 dark:bg-primary-950 [&>*]:m-4">
-	<ThemeSwitcher />
-	<DarkModeToggle />
-	<h1 class="text-primary-900 dark:text-primary-100 text-9xl md:p-48">Hello!</h1>
+<main
+	class="dark:bg-primary-950 bg-primary-100 min-w-screen flex min-h-screen flex-col items-center [&>*]:m-4">
+	<header>
+		<ThemeSwitcher />
+		<DarkModeToggle />
+	</header>
+	<h1 class="text-primary-900 dark:text-primary-100 text-9xl md:p-48 md:pb-0">Hello!</h1>
+	<p class="text-primary-900 dark:text-primary-100">This is the {$color} theme.</p>
+	<button class="btn-primary">A {$color} button</button>
+	<button class="btn-secondary">A {$color} secondary button</button>
 </main>
 
 <Footer />
--- uno.config.js
+++ uno.config.js
@@ -9,9 +9,9 @@ import presetTheme from "unocss-preset-theme";
 export default defineConfig({
 	shortcuts: {
 		"btn-primary":
-			"bg-primary-400 outline-primary-500 outline-2 text-base-100 dark:bg-primary-700 dark:active:(bg-primary-600 translate-y-1) transition-transform active:(bg-primary-100 translate-y-1) dark:hover:bg-primary-600 hover:bg-primary-200 p-2 rounded-lg shadow-md",
+			"bg-primary-400 outline-primary-500 outline-2 text-base-100 dark:bg-primary-700 dark:active:(bg-primary-600 translate-y-1) transition-transform active:(bg-primary-100 translate-y-1) dark:hover:bg-primary-600 hover:bg-primary-300 p-2 rounded-lg shadow-md",
 		"btn-secondary":
-			"bg-secondary-400 outline-secondary-500 outline-2 text-base-100 dark:bg-secondary-700 dark:active:(bg-secondary-600 translate-y-1)  transition-transform active:(bg-secondary-100 translate-y-1)  dark:hover:bg-secondary-600 hover:bg-secondary-200 p-2 rounded-lg shadow-md",
+			"bg-secondary-400 outline-secondary-500 outline-2 text-base-100 dark:bg-secondary-700 dark:active:(bg-secondary-600 translate-y-1)  transition-transform active:(bg-secondary-100 translate-y-1)  dark:hover:bg-secondary-600 hover:bg-secondary-300 p-2 rounded-lg shadow-md",
 	},
 	safelist: [
 		...Array.from({ length: 26 }, (_, i) => String.fromCharCode("a".charCodeAt(0) + i)).map(
