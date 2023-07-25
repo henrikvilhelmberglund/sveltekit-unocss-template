a9673fbcdecd7502f996350b7ef81484885b6150
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-07-25 13:03:17 +0200

    feat: :sparkles: use UnoCSS Vite plugin instead

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,12 @@
 import { sveltekit } from "@sveltejs/kit/vite";
+import UnoCSS from "@unocss/svelte-scoped/vite";
 
 import { defineConfig } from "vite";
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [
+		UnoCSS({
+      injectReset: "@unocss/reset/tailwind.css",
+		}),
+		sveltekit(),
+	],
 });
