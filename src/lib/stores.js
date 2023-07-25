f27d34e9b98569011428b65e59d3d4fffb890187
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-07-25 15:40:09 +0200

    feat: :art: move files to subfolder

--- src/lib/stores.js
+++
@@ -1,5 +0,0 @@
-import { writable } from "svelte/store";
-import { persisted } from "svelte-local-storage-store";
-
-export const activeTheme = writable("");
-export const preferredMode = persisted("preferredMode", "");
