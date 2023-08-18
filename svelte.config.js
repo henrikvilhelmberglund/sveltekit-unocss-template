1d06d9fdf2683741f46d7460b5b6325e05164569
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-08-18 17:31:33 +0200

    feat: :package: add Melt

--- svelte.config.js
+++ svelte.config.js
@@ -4,16 +4,14 @@
 // import { vitePreprocess } from "@sveltejs/kit/vite";
 import adapter from "@sveltejs/adapter-static";
 import { vitePreprocess } from "@sveltejs/kit/vite";
-
-
+import { preprocessMeltUI } from "@melt-ui/pp";
+import sequence from "svelte-sequential-preprocessor";
 
 const dev = process.argv.includes("dev");
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: [
-		vitePreprocess(),
-  ],
+	preprocess: sequence([vitePreprocess(), preprocessMeltUI()]),
 	kit: {
 		adapter: adapter(),
 	},
